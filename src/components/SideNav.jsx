409c486030dd73f8d016e9b0106414974a15b348
Tony-123-a11y <ayushsinghatschool@gmail.com>
2025-03-26 22:23:03 +0530

    third

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
 import Heading from './Heading'
-import { motion } from 'motion/react'
+import { easeIn, motion } from 'motion/react'
+import { easeOut } from 'motion'
 
 
 const SideNav = () => {
+  let arr=[
+    {
+    title:'Home',
+    id:'home'
+    },
+    {
+    title:'About',
+    id:'about'
+    },
+    {
+    title:'Skills',
+    id:'skill'
+    },
+    // {
+    // title:'Achievements',
+    // id:'achievement'
+    // },
+    {
+    title:'Projects',
+    id:'project'
+    },
+    {
+    title:'contact',
+    id:'contact'
+    },
+      ]
+      function goToSection(id){
+        document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start" });
+      }
   return (
-    <div className="fixed top-[55px] left-0 w-full h-[calc(100vh-74px)] z-100 backdrop-blur-3xl ">
+    <motion.div 
+    initial={{
+      opacity:0
+    }}
+    animate={{
+      opacity:1
+    }}
+    className="fixed top-[55px] left-0 w-full  h-[calc(100vh-74px)] bg-black/60 z-20">
       <motion.div
       initial={{
-       translateY:-100
+       translateY:-50
       }
       }
       animate={{
@@ -16,13 +53,22 @@ const SideNav = () => {
       
       }}
       transition={{
-        duration:.5
+        duration:.2,
+        ease:easeOut
         
       }}
+      className=' bg-white   relative'
       >
-        <Heading heading={'projects'}/>
+       {/* <h1 className='py-4 px-5 text-xl font-medium'>Routes</h1> */}
+       <ul className='text-center py-6 space-y-1'>
+       {
+              arr.map((ele,i)=>{
+               return  <li key={i} onClick={()=>goToSection(ele.id)} className='px-5 shadow-sm  py-3 cursor-pointer relative capitalize '>{ele.title}</li>
+              })
+            }
+            </ul>
       </motion.div>
-    </div>
+    </motion.div>
   )
 }
 
